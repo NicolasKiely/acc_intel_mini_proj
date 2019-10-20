e17ddef3cf3df9214cda973d663e78e44296e40a
Nicolas Kiely <mitchell3141@gmail.com>
2019-10-19 23:52:24 -0400

    Issue #4: Import CSV data
    Create initial movie records

--- src/model/movie.py
+++ src/model/movie.py
@@ -1,15 +1,18 @@
 """ Top level models for movie records and stats
 
 Unnormalized movie fields:
-    color, director_name, num_critic_for_reviews, duration,
+    director_name, num_critic_for_reviews, duration,
     director_facebook_likes, actor_3_facebook_likes, actor_2_name,
     actor_1_facebook_likes, gross, genres, actor_1_name, movie_title,
     num_voted_users, cast_total_facebook_likes, actor_3_name,
     facenumber_in_poster, plot_keywords, movie_imdb_link, num_user_for_reviews,
     language, country, content_rating, budget, title_year,
     actor_2_facebook_likes, imdb_score, aspect_ratio, movie_facebook_likes
+
+Normalized movie fields:
+    color
 """
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, UniqueConstraint
 from sqlalchemy.orm import relationship
 
 from src.model import db
@@ -22,11 +25,20 @@ class Movie(db.ModelBase):
     #: Primary key
     pk = Column(Integer, primary_key=True, autoincrement=True)
 
+    #: Title to movie
+    movie_title = Column(String(250), nullable=False)
+
+    #: Title year (note: empty year should be treated as empty string)
+    title_year = Column(String(16), nullable=False)
+
     #: Foreign key to movie color
     movie_color_pk = Column(Integer, ForeignKey('movie_colors.pk'))
 
     #: Movie color relationship
     movie_color = relationship('MovieColor', back_populates='movies')
+
+    #: Movies are uniquely identified by title and year
+    UniqueConstraint('movie_title', 'title_year')
 
 
 class MovieColor(db.ModelBase):
--- src/view/cli/load_data.py
+++ src/view/cli/load_data.py
@@ -1,5 +1,6 @@
 import logging
 import pandas as pd
+import sqlalchemy
 from typing import List
 
 import src.model.db
@@ -29,16 +30,20 @@ class LoadDataView(cli_view.CliView):
         logger.info('Loading file "%s"' % file_name)
         data = src.utils.load_df_from_dataset(file_name)
 
-        # Build db
+        # Build db and get session
         engine = src.model.db.EngineWrapper.get_engine()
         src.model.db.ModelBase.metadata.create_all(engine)
-
         session = src.model.db.EngineWrapper.get_session()
+
+        # Process movie fields
         process_movie_colors(session, data)
 
+        # Process move record itself
+        process_movie_records(session, data)
 
-def process_movie_colors(session, data):
-    """ Processes list of unique colors in database """
+
+def process_movie_colors(session, data: pd.DataFrame):
+    """ Adds list of unique colors in database from data input """
     movie_colors = data['color'].unique()
     for movie_color_raw in movie_colors:
         # Clean up color name
@@ -57,5 +62,70 @@ def process_movie_colors(session, data):
             logger.info('Creating new movie_color record "%s"' % movie_color)
             new_color_record = src.model.movie.MovieColor(color=movie_color)
             session.add(new_color_record)
+
+    session.commit()
+
+
+def process_movie_records(session, data: pd.DataFrame):
+    """ Adds list of movie records to database from data input """
+    # Lookup of movie record number by title+year
+    movie_title_index = {}
+    for i, record in data.iterrows():
+        record_no = i+1
+
+        # Get searchable title+year of movie record
+        movie_title = record['movie_title'].strip()
+        movie_title_l = movie_title.lower()
+        movie_year = record['title_year']
+
+        if pd.isna(movie_title_l):
+            logger.warning(
+                'Movie with no title on record #%s' % record_no
+            )
+            continue
+
+        if pd.isna(movie_year):
+            logger.warning(
+                'Movie with no year on record #%s' % record_no
+            )
+            movie_year = ''
+
+        else:
+            movie_year = str(movie_year)
+
+        movie_record_index = (movie_title_l, movie_year)
+
+        if movie_record_index in movie_title_index:
+            logger.warning(
+                'Duplicate movie "%s" (#%s, #%s)' % (
+                    movie_title, movie_title_index[movie_title_l], record_no
+                )
+            )
+            continue
+
+        else:
+            # Mark movie by record number in dataframe
+            movie_title_index[movie_title_l] = record_no
+
+        # Check if record exists in db by name
+        old_movie_record = session.query(
+            src.model.movie.Movie
+        ).filter(
+            sqlalchemy.func.lower(src.model.movie.Movie.movie_title)
+            == movie_title_l
+        ).filter(
+            src.model.movie.Movie.title_year == movie_year
+        ).one_or_none()
+
+        if old_movie_record is None:
+            # No record; create
+            logger.info('Creating new movie record "%s"' % movie_title)
+            movie_record = src.model.movie.Movie(
+                movie_title=movie_title, title_year=movie_year
+            )
+            session.add(movie_record)
+
+        else:
+            movie_record = old_movie_record
 
     session.commit()
